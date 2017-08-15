4c8650f0cae61688f061960f43162c01f48c8311
Sid Reed <sidreed34@gmail.com>
2017-08-15 17:53:07 -0400

    added comments to explain how it works

--- scripts/matchingprotid.py
+++ scripts/matchingprotid.py
@@ -1,28 +1,28 @@
 #!/usr/local/bin/python3.5
 #Author: Ann Le, McMaster University
 #retrieves the sequences from a list of protIDs to create a fasta file for the oneway NR blast
-#argv[2] is concatenated protein fasta file, used for blast
-#argv[1] is a list of protein IDs, the genesForNR.txt file
+#argv[1] is concatenated protein fasta file, used for blast
+#argv[2] is a list of protein IDs, the genesForNR.txt file
 #file prints, redirect the output to whatever output file desired
 import re
 import sys
 
 protids = []
 
-with open(sys.argv[2], 'r') as protidfile:
+with open(sys.argv[2], 'r') as protidfile: # creates a python list of all the desired protein ids you want to get sequences for
 	for line in protidfile.readlines():
 		protids.append(line)
 
-compil_match = re.compile('^>')
+compil_match = re.compile('^>') # matches headers in a fasta file
 
 with open(sys.argv[1], 'r') as seqidfile:
 	for seqline in seqidfile:
-		if any(pid in seqline for pid in protids):
+		if any(pid in seqline for pid in protids): # if any of the protID are in the fasta header
 			print(seqline, end="")
-			for seqline in seqidfile:
-				if re.match(compil_match, seqline) == None:
+			for seqline in seqidfile: # print the sequence lines 
+				if re.match(compil_match, seqline) == None: #if the line is not a fasta header
 					print(seqline, end="")
-				else:
-					break
+				else: # once you hit a fasta header
+					break # break out of this loop and move onto the next seqline
 
 
