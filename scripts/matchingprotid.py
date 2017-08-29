01f6ff2686826ec85a1761fb07bc841964b48aec
Sid Reed <sidreed34@gmail.com>
2017-08-29 16:21:57 -0400

    reworked it, changed some comments, now it works for sure

--- scripts/matchingprotid.py
+++ scripts/matchingprotid.py
@@ -10,19 +10,30 @@ import sys
 protids = []
 
 with open(sys.argv[2], 'r') as protidfile: # creates a python list of all the desired protein ids you want to get sequences for
-	for line in protidfile.readlines():
-		protids.append(line)
+    for line in protidfile.readlines():
+        protids.append(line.strip())
 
 compil_match = re.compile('^>') # matches headers in a fasta file
 
-with open(sys.argv[1], 'r') as seqidfile:
-	for seqline in seqidfile:
-		if any(pid in seqline for pid in protids): # if any of the protID are in the fasta header
-			print(seqline, end="")
-			for seqline in seqidfile: # print the sequence lines 
-				if re.match(compil_match, seqline) == None: #if the line is not a fasta header
-					print(seqline, end="")
-				else: # once you hit a fasta header
-					break # break out of this loop and move onto the next seqline
+seq = open(sys.argv[1], 'r').readlines()
+
+for i in range(len(seq)-2):
+    if any(pid in seq[i] for pid in protids):
+        for j in range(i,len(seq)-2):
+            if re.match(compil_match, seq[j+1]) == None:
+                print(seq[j], end="") # all lines in the sequence except for last one
+            else:
+                print(seq[j], end="") # last line of sequence
+                break
 
 
+
+#with open(sys.argv[1], 'r') as seqidfile:
+#    for seqline in seqidfile:
+#        if any(pid in seqline for pid in protids): # if any of the protID are in the fasta header
+#            print(seqline, end="")
+#            for seqline in seqidfile: # print the sequence lines 
+#                if re.match(compil_match, seqline) == None: #if the line is not a fasta header
+#                    print(seqline, end = "")
+#                else: # once you hit a fasta header
+#                    break # break out of this loop and move onto the next seqline
