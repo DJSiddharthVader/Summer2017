6596a980fa5c41b144460bfcfea923ef3599da1b
Sid Reed <sidreed34@gmail.com>
2017-11-14 10:30:44 -0500

    commented out tree finding section, needs to be reevaluated

--- scripts/createPresenceAbsencemtrix.R
+++ scripts/createPresenceAbsencemtrix.R
@@ -104,35 +104,35 @@ print(buildMend-buildMstart)
 
 
 
-treestart <- Sys.time()
-#------------Extracting Families to Create Tree-----------------------------------------
-
-eqs <- function(x,y) if (x==y) x else FALSE #function for checking if two elemnts are equal
-cols <- list() #initializes empty list
-for (cl in 1:ncol(presenceAbsence)){ #for each family in the PA matrix (families are colums)
-    if (presenceAbsence[1,cl]==1){ # if the value of the matrix for the first row in that colum is 1
-        cols <- c(cols,ifelse(Reduce(eqs,presenceAbsence[,cl]),cl,FALSE)) # check if every other row is 1, if so append the colum number to cols
-    } else { # otherwise append FALSE
-        cols <- c(cols,FALSE)
-    }
-}
-
-treefamilies <- genefamilies[unlist(cols)] # gets all the families present in every organism
-treeMembers <- data.frame() # initalize empty df for the single members in the tree
-
-for (fam in 1:length(treefamilies)){ # for every family in treefamilies
-    treeMembers[fam,1] <- treefamilies[[fam]][1] # get the first protein in that family, put it in tree members
-}
-
-write.table(treeMembers, file=paste0(as.character(args[3]), "TreeMembers.txt"), row.names=FALSE, col.names=FALSE, quote=FALSE) #write all the treeMembers as a txt file
-#----------------------------------------------------------------------------------------
-treeend <- Sys.time()
+#treestart <- Sys.time()
+##------------Extracting Families to Create Tree-----------------------------------------
+#
+#eqs <- function(x,y) if (x==y) x else FALSE #function for checking if two elemnts are equal
+#cols <- list() #initializes empty list
+#for (cl in 1:ncol(presenceAbsence)){ #for each family in the PA matrix (families are colums)
+#    if (presenceAbsence[1,cl]==1){ # if the value of the matrix for the first row in that colum is 1
+#        cols <- c(cols,ifelse(Reduce(eqs,presenceAbsence[,cl]),cl,FALSE)) # check if every other row is 1, if so append the colum number to cols
+#    } else { # otherwise append FALSE
+#        cols <- c(cols,FALSE)
+#    }
+#}
+#
+#treefamilies <- genefamilies[unlist(cols)] # gets all the families present in every organism
+#treeMembers <- data.frame() # initalize empty df for the single members in the tree
+#
+#for (fam in 1:length(treefamilies)){ # for every family in treefamilies
+#    treeMembers[fam,1] <- treefamilies[[fam]][1] # get the first protein in that family, put it in tree members
+#}
+#
+#write.table(treeMembers, file=paste0(as.character(args[3]), "TreeMembers.txt"), row.names=FALSE, col.names=FALSE, quote=FALSE) #write all the treeMembers as a txt file
+##----------------------------------------------------------------------------------------
+#treeend <- Sys.time()
 print("# of families:")
 print(length(genefamilies))
 print("# of organisms")
 print(length(orgenes))
-print("time to get tree families:")
-print(treeend-treestart)
+#print("time to get tree families:")
+#print(treeend-treestart)
 print("total run time:")
 end <- Sys.time()
 print(end-start)
